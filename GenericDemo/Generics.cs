009b1e0cc9182249dee522869152804e0dc5df59
unknown <satya-1298>
2023-06-29 21:32:54 +0530

    [Satya]Add:generic program

---
+++ GenericDemo/Generics.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDemo
+{
+    //Generic means general form
+    //It does not specify pariticular datatype
+    //we are using <> with type name
+    //TypeName <T> -T is your type parameter
+    //Generic allow us to a single class or method that can be used in different datatype
+    //Advantages - we can reuse the code
+    public class Generics<T>
+    {
+        public T data;
+        public Generics(T data) 
+        {
+            this.data = data;
+            Console.WriteLine(data);
+        }
+        public void Demo(T num)
+        {
+            Console.WriteLine(num);
+        }
+        public void Demo_Two(int num)
+        {
+            Console.WriteLine(num);
+        }
+    }
+    public class Generics
+    {
+        public void Demo<T>(T num) 
+        {
+              Console.WriteLine(num); 
+        }
+        public void Demo_one(int num)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
---
+++ GenericDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDemo
+{
+    public  class Program
+    {
+        public static void Main(string[] args)
+        {
+            Generics<int> generics = new Generics<int>(25);
+            Generics<char> genericschar = new Generics<char>('S');
+            Generics<string> genericstring = new Generics<string>("Satya");
+            Generics<bool> genericsbool = new Generics<bool>(true);
+            Generics<double> genericsdouble = new Generics<double>(2.544234);
+            Generics<float> genericsfloat = new Generics<float>(2.4f);
+            generics.Demo_Two(4);
+            Console.WriteLine("Enter a Value");
+             var num = Convert.ToInt32(Console.ReadLine());
+            generics.Demo(num);
+            genericstring.Demo("satya");
+            genericschar.Demo('d');
+            Console.WriteLine("***************");
+            Generics obj = new Generics();
+            obj.Demo<int>(32);
+            obj.Demo<string>("hello");
+            obj.Demo<bool>(false);
+            obj.Demo_one(3);
+           Console.ReadLine(); 
+        }
+    }
+}
